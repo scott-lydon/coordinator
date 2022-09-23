a90734f212128bf20954754f407e9ae6d08e8423
Scott Lydon <scottlydon18@gmail.com>
2022-09-22 21:45:41 -0400

    Convert to encapsulation from Coordinator.

--- coordinator/SceneDelegate.swift
+++ coordinator/SceneDelegate.swift
@@ -10,8 +10,6 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    var coordinator: MainCoordinator?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -19,9 +17,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
-        let navController = UINavigationController()
-        coordinator = MainCoordinator(navigationController: navController)
-        coordinator?.start()
+        let viewController = ViewController.instantiate()
+        viewController.view.backgroundColor = .green
+        let navController = UINavigationController(rootViewController: viewController)
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = navController
         window?.makeKeyAndVisible()
